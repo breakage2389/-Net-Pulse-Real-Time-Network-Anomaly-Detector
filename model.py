de8a9b4c5dcbb6d8df40498848a9471065a6cb7c
niksata <niki.kolev2006@gmail.com>
2026-02-01 14:46:41 +0200

    sniff and extract are done

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
 from sklearn.ensemble import IsolationForest
 import pandas as pd
 
+model = IsolationForest(contamination=0.05 , random_state=42)
+
+def train_model(training_data):
+    model.fit(training_data[['size' , 'protocol']])
+    print('Brain has been trained on normal traffic.')
+
+def predict_model(training_data):
+    prediction = model.predict(training_data[['size' , 'protocol']])
+    return prediction[0]
